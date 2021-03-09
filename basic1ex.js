e3ebc8094664ffba7f9bcb856dae5daa848e62d8
Agnes Nowak <aganowak1211@gmail.com>
2021-03-09 11:56:55 +0100

    add basic1

---
+++ basic1ex.js
@@ -0,0 +1,26 @@
+// If the length of your name is larger than 5 characters, change the text colour to green. Otherwise, change the text-colour to red.  -->
+// var nameLength = document.querySelectorAll(".lastName").length
+// for (var i=0; )
+
+// var inputArray = document.querySelector("input");
+// console.log(inputArray);
+
+
+document.getElementById("lastName").addEventListener("input", function() {
+
+    inputLength
+
+});
+
+// function inputLength (array) {
+//     for (let i=0; i<array.lenght; i++ ){
+//         if (i<6) {
+//             red
+//         }
+//         else (i>=6) {
+//             turn green
+//         }
+//     }
+  
+// }
+
