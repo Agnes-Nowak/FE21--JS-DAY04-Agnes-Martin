48361ea0a1b46b006803319825c47c7ab33f54b5
Martin Obermayer <59625525+mobermayer@users.noreply.github.com>
2021-03-09 13:46:49 +0100

    update: ex1 basic

--- basic1ex.js
+++ basic1ex.js
@@ -1,37 +1,11 @@
-// If the length of your name is larger than 5 characters, change the text colour to green. Otherwise, change the text-colour to red.  -->
-// var nameLength = document.querySelectorAll(".lastName").length
-// for (var i=0; )
-
-// var inputArray = document.querySelector("input");
-// console.log(inputArray);
-
-
-// document.getElementById("lastName").addEventListener("input", function() {
-
-//     inputLength
-
-// });
-
-// function inputLength (array) {
-//     for (let i=0; i<array.lenght; i++ ){
-//         if (i<6) {
-//             red
-//         }
-//         else (i>=6) {
-//             turn green
-//         }
-//     }
-  
-// }
-
-var elementLastName =  document.getElementById("lastName");
+var elementLastName = document.getElementById("lastName");
 elementLastName.addEventListener("input", getInput);
 
 function getInput() {
     var valueLastName = elementLastName.value;
     document.getElementsByTagName("div")[0].innerHTML = valueLastName;
 
-    if(5 <= valueLastName.length) {
+    if (5 <= valueLastName.length) {
         document.getElementsByTagName("div")[0].style.color = "green";
     }
     else {
